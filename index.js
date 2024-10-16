c3420bc6f34aa66d5a87fed39edb84dc2acbaa83
ktm526 <ktm0526@postech.ac.kr>
2024-10-16 18:06:36 +0900

    init3

--- index.js
+++ index.js
@@ -1,29 +1,56 @@
+// main.js
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 function createWindow() {
+  console.log('Creating BrowserWindow...');
   const win = new BrowserWindow({
     width: 1024,
     height: 600,
     webPreferences: {
-      contextIsolation: true,
-      enableRemoteModule: false,
-      //preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, 'preload.js'),
     },
   });
 
-  win.loadFile(path.join(__dirname, 'index.html'));
+  win.loadFile('index.html')
+    .then(() => {
+      console.log('index.html loaded successfully.');
+    })
+    .catch((err) => {
+      console.error('Failed to load index.html:', err);
+    });
 }
 
 app.whenReady().then(() => {
-  console.log("app ready");
+  console.log('App is ready.');
   createWindow();
 
   app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) {
+      console.log('Activating app and creating window.');
+      createWindow();
+    }
   });
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+  console.log('All windows closed.');
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+app.on('ready', () => {
+  console.log('App ready event triggered.');
+});
+
+app.on('will-finish-launching', () => {
+  console.log('App will finish launching event triggered.');
+});
+
+app.on('before-quit', () => {
+  console.log('App is about to quit.');
+});
+
+app.on('quit', () => {
+  console.log('App quit event triggered.');
 });
